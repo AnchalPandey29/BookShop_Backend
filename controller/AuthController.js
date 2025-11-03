eb7fca9c9e9ae99a68683b18e90c0ce057aa2fce
AnchalPandey29 <anchal29pandey@gmail.com>
2025-11-03 17:00:14 +0530

    Changes added

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,41 +1,67 @@
 const User = require("../models/UserModel");
 
-// create user
-const register = async(req,res)=>{
-    try{
-        const {firebaseUid,email,name,photoURL,phoneNumber,location}=req.body;
+// Register or update user
+const register = async (req, res) => {
+  try {
+    const firebaseUid = req.user.uid; 
+    const { name, email, picture, phone_number } = req.user;
+    console.log(req.user);
+    
 
-        if(!firebaseUid||!email)
-        {
-            return res.status(400).json({message:"Missing required fields"})
-        }
+    if (!firebaseUid || !email)
+      return res.status(400).json({ message: "Missing required fields" });
 
-        const user = await User.findOneAndUpdate({firebaseUid},{name,email,photoURL,phoneNumber,location},
-            {new:true, upsert:true}
-        );
+    const user = await User.findOneAndUpdate(
+      { firebaseUid },
+      {
+        firebaseUid,
+        name: name || req.body.name,
+        email,
+        photoURL: picture || req.body.photoURL,
+        phoneNumber: phone_number || req.body.phoneNumber,
+        location: req.body.location,
+      },
+      { new: true, upsert: true }
+    );
 
-        res.status(200).json({success:true, user})
+    res.status(200).json({ success: true, user });
+  } catch (err) {
+    console.error("Register Error:", err.message);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Fetch user details
+const getUser = async (req, res) => {
+  try {
+    const firebaseUid = req.user.uid;
+    const user = await User.findOne({ firebaseUid });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ success: true, user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//login method
+const login = async (req, res) => {
+  try {
+    const firebaseUid = req.user.uid;
+    const user = await User.findOne({ firebaseUid });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found. Please sign up first." });
     }
-    catch(err)
-    {
-     res.status(500).json({message: err.message})
 
-    }
-}
+    res.status(200).json({ success: true, user });
+  } catch (err) {
+    console.error("Login Error:", err.message);
+    res.status(500).json({ message: err.message });
+  }
+};
 
-const getUser = async(req,res)=>{
-    try{
-        const firebaseUid = req.user.uid;
-        const user= User.findOne(firebaseUid);
-        if(!user) return res.status(404).json({message:"User not found"})
 
-        res.status(200).json({message:"User data", user});
-
-    }
-    catch(err)
-    {
-          res.status(500).json({message: err.message})
-    }
-}
-
-module.exports={register, getUser};
+module.exports = { register, getUser,login };
--- router/AuthRouter.js
+++ router/AuthRouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {register, getUser} =require("../controller/AuthController");
-const { verifyToken } = require("../middleware/AuthMiddleware");
+const {register, getUser,login} =require("../controller/AuthController");
+const {verifyToken} = require("../middleware/AuthMiddleware")
 
 
 router.post("/",verifyToken, register);
 router.get("/",verifyToken,getUser);
+router.post("/login", verifyToken, login);
 
 module.exports = router;
 
